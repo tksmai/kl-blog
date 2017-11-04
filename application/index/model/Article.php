4ebe14cb9409c14eaca3ca2a1d4c050b568d71fe
Tsukasa <tsukasa.kzk@gmail.com>
2017-11-04 20:56:41 +0800

    first commit

---
+++ application/index/model/Article.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\index\model;
+
+class Article
+{
+    public static function list()
+    {
+
+    }
+
+    public static function read()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function del()
+    {
+        
+    }
+}
---
+++ application/index/model/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\index\model;
+
+class User
+{
+    private $username = '';
+    private $userId = 0;
+    
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function saveSession()
+    {
+
+    }
+}
