b2532fa324224e6963b54089293710eebef69171
Tsukasa <tsukasa.kzk@gmail.com>
2017-11-19 10:13:45 +0800

    add post with fake user

--- application/index/controller/Post.php
+++ application/index/controller/Post.php
@@ -9,6 +9,14 @@ use think\Response;
 
 class Post
 {
+    /**
+     * GET /Post
+     * 定义为获取Post列表
+     *
+     * @param Request $request
+     * @return Response
+     * Kanzaki Tsukasa
+     */
     public function index(Request $request): Response
     {
         $page = $request->get('p/d', 1);
@@ -17,13 +25,30 @@ class Post
         return json($list, 200);
     }
 
+    /**
+     * GET /Post/:id
+     * 定义为获取单个Post
+     *
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     * Kanzaki Tsukasa
+     */
     public function read(Request $request, int $id): Response
     {
-        $item = Posts::read($id);
+        $item = Posts::read($id, Posts::STATUS_PUBLISHED);
         $code = $item ? 200 : 204;
         return json($item, $code);
     }
 
+    /**
+     * POST /Post
+     * 定义为新增Post
+     *
+     * @param Request $request
+     * @return Response
+     * Kanzaki Tsukasa
+     */
     public function save(Request $request): Response
     {
         $data = $request->post();
@@ -34,7 +59,9 @@ class Post
             return json(['msg' => $validate->getError()], 400);
         }
         $post = new Posts($data);
-        if ($post->add(1)) {
+        // 获取用户资料，此处写死1
+        $user = ['user_id', 1];
+        if ($post->add($user['user_id'])) {
             $rec = ['msg' => 'success', 'post_id' => $post->getId()];
             $code = 200;
         } else {
