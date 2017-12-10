c9e02b1b038996129d66eea089651333a72e4d7b
Tsukasa <tsukasa.kzk@gmail.com>
2017-12-10 09:37:30 +0800

    user register and response code helper

---
+++ application/common/helper/Response.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\common\helper;
+
+class Response
+{
+    const CODE_ACTION_SUCCESS = 200;
+    const CODE_CREATE_SUCCESS = 201;
+    const CODE_UPDATE_SUCCESS = 200;
+    const CODE_ACTION_FAILED = 500;
+    const CODE_REQ_PARAM_ERR = 400;
+}
--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -2,6 +2,8 @@
 
 namespace app\index\controller;
 
+use app\common\helper\Response as RH;
+use app\index\model\Users;
 use think\Loader;
 use think\Request;
 use think\Response;
@@ -21,8 +23,22 @@ class User
         // 验证用户名和密码
         $validate = Loader::validate('Users');
         if (!$validate->check($data)) {
-            return json(['msg' => $validate->getError()], 400);
+            return json(['msg' => $validate->getError()], RH::CODE_REQ_PARAM_ERR);
         }
-        return json(['msg' => '注册成功'], 200);
+        if (Users::register($data['username'], $data['password'])) {
+            return json(['msg' => '注册成功'], 200);
+        } else {
+            switch (Users::getErrorCode()) {
+                case Users::ERR_USERNAME_EXIST:
+                    $msg = '用户已存在';
+                    break;
+
+                default:
+                    $msg = Users::getErrorMsg();
+                    break;
+            }
+            return json(['msg' => $msg, 'code' => Users::getErrorCode()], RH::CODE_ACTION_FAILED);
+        }
+
     }
 }
